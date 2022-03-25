8810296ad1c521baf7245990b66a89202cf23e0d
Amin Shali <amshali@gmail.com>
2022-03-24 17:11:38 -0700

    Added useful functions to Matrix

--- src/main/java/sutton/barto/rlbook/Matrix.java
+++ src/main/java/sutton/barto/rlbook/Matrix.java
@@ -1,6 +1,8 @@
 package sutton.barto.rlbook;
 
 import java.util.Vector;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Matrix<T> {
 
@@ -17,6 +19,26 @@ public class Matrix<T> {
         data.get(i).addElement(init);
       }
     }
+  }
+
+  public Matrix(Matrix<T> o) {
+    this.rows = o.rows;
+    this.columns = o.columns;
+    data = new Vector<>();
+    for (int i = 0; i < rows; i++) {
+      data.addElement(new Vector<>());
+      for (int j = 0; j < columns; j++) {
+        data.get(i).addElement(o.get(i, j));
+      }
+    }
+  }
+
+  public Stream<T> row(int i) {
+    return data.get(i).stream();
+  }
+
+  public Stream<T> column(int c) {
+    return IntStream.range(0, rows()).mapToObj(i -> get(i, c));
   }
 
   public T get(int i, int j) {
